69d8720abf9eb392c66d35f6b582924f685cab7e
Michael Rhodes <mrhodes@mediapolis.com>
2021-01-30 22:08:58 -0400

    update outdated packages

--- models/user.js
+++ models/user.js
@@ -1,71 +1,81 @@
 const debug = require('debug')('marlin-db')
 const Sequelize = require('sequelize')
 
-module.exports = (sequelize, DataTypes) => {
-	const User = sequelize.define('User', {
-		id: {
-			type: Sequelize.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		name: {
-			type: Sequelize.STRING,
-			allowNull: true,
-			len: [0, 60]
-		},
-		username: {
-			type: Sequelize.STRING,
-			unique: true,
-			allowNull: false,
-			len: [4, 20],
-			is: ['^[a-zA-Z0-9-]+$', '']
-		},
-		email: {
-			type: Sequelize.STRING,
-			unique: true,
-			allowNull: false,
-			isEmail: true
-		},
-		password: {
-			type: Sequelize.STRING,
-			unique: true,
-			allowNull: false,
-			ADMIN: {
-				hidden: true
-			}
-		},
-		profilePhoto: {
-			type: Sequelize.JSON,
-			allowNull: true,
-			ADMIN: {
-				inputType: 'image',
-				accepts: 'image/*'
-			}
-		},
-		validated: {
-			type: Sequelize.INTEGER,
-			allowNull: true
-		},
-		stripeCustomer: {
-			type: Sequelize.STRING,
-			allowNull: true
-		},
-		stripeSubscription: {
-			type: Sequelize.STRING,
-			allowNull: true
-		},
-		stripeStatus: {
-			type: Sequelize.STRING,
-			allowNull: true
-		},
-		pendingEmail: {
-			type: Sequelize.STRING,
-			unique: true,
-			allowNull: true,
-			isEmail: true
+module.exports.name = 'User'
+
+const definition = {
+	id: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	name: {
+		type: Sequelize.STRING,
+		allowNull: true,
+		len: [0, 60]
+	},
+	username: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false,
+		len: [4, 20],
+		is: ['^[a-zA-Z0-9-]+$', '']
+	},
+	email: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false,
+		isEmail: true
+	},
+	password: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false,
+		ADMIN: {
+			hidden: true
 		}
-	}, {
+	},
+	profilePhoto: {
+		type: Sequelize.JSON,
+		allowNull: true,
+		ADMIN: {
+			inputType: 'image',
+			accepts: 'image/*'
+		}
+	},
+	validated: {
+		type: Sequelize.INTEGER,
+		allowNull: true
+	},
+	stripeCustomer: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	stripeSubscription: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	stripeStatus: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	pendingEmail: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: true,
+		isEmail: true
+	}
+}
+
+module.exports.extend = (columnDefs) => {
+	for (const prop in columnDefs) {
+		definition[prop] = columnDefs[prop]
+	}
+}
+
+module.exports.define = (sequelize, DataTypes) => {
+	const User = sequelize.define('User', definition, {
 		ADMIN: {
 			defaultColumn: 'email',
 			listColumns: ['email', 'username', 'validated'],
